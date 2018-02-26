d0625ea651758100a1f711571df30e038c35ab7e
AAtanasov <allen@htmlburger.com>
2018-02-26 21:49:30 +0200

    Add an API router for editing a note

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Http\Request;
 Route::get('notes', 'ApiController@getNotes');
 Route::post('addNote', 'ApiController@addNote');
 Route::delete('deleteNote/{note}', 'ApiController@deleteNote');
+Route::patch('updateTask/{note}', 'ApiController@updateTask');
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
